f8ca215e944f0b7ce66963dd9cd8a8c66e61b3e7
Eduardo <florianoeduardo03@gmail.com>
2022-12-19 03:15:16 -0300

    primeiro commit

---
+++ Testes-REST/src/main/java/br/com/mesttra/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+package br.com.mesttra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaPrincipal {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramaPrincipal.class, args);
+    }
+
+}
---
+++ Testes-REST/src/test/java/br/com/mesttra/ProgramaPrincipalTests.java
@@ -0,0 +1,13 @@
+package br.com.mesttra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaPrincipalTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
