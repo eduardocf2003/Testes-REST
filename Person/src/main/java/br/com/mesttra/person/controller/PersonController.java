73e174b5b1e6efee43a7381c76db15716d759622
Eduardo <florianoeduardo03@gmail.com>
2022-12-28 02:45:04 -0300

    CRUD person

--- Person/src/main/java/br/com/mesttra/person/controller/PersonController.java
+++ Person/src/main/java/br/com/mesttra/person/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -21,13 +22,47 @@ public class PersonController {
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(
-            @PathVariable(value = "id") String id) throws Exception {
+            @PathVariable(value = "id") String id) {
 
 
         return personService.findById(id);
 
     }
 
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
 
 
+        return personService.findAll();
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+
+
+        return personService.createPerson(person);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+
+
+        return personService.updatePerson(person);
+
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+
+        personService.delete(id);
+
+    }
 }
--- Person/src/main/java/br/com/mesttra/person/service/PersonService.java
+++ Person/src/main/java/br/com/mesttra/person/service/PersonService.java
@@ -3,6 +3,8 @@ package br.com.mesttra.person.service;
 import br.com.mesttra.person.entity.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,19 @@ public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<Person> findAll() {
+
+        logger.info("Encontrando todas as pessoas!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+
+    }
 
     public Person findById(String id) {
 
@@ -23,6 +38,38 @@ public class PersonService {
         person.setAddress("São josé dos campos");
         person.setGender("Homem");
         return person;
+    }
+
+    private Person mockPerson(int i) {
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some Address in Brasil " + i);
+        person.setGender("Homem");
+        return person;
+
+    }
+
+    public Person createPerson(Person person) {
+
+        logger.info("Criando uma pessoa!");
+
+        return person;
+    }
+
+    public Person updatePerson(Person person) {
+
+        logger.info("Atualizando uma pessoa!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+
+        logger.info("Removendo uma pessoa!");
+
     }
 
 }
